bbbb5dfd7f28aa661890f5113942a867f7f6c13b
benwilcock <benwilcock@gmail.com>
2015-09-18 16:31:56 +0100

    Added the first code from the getting started tutorial (gradle).

---
+++ src/main/java/hello/HelloWorldConfiguration.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ben on 18/09/15.
+ */
+@SpringBootApplication
+public class HelloWorldConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldConfiguration.class, args);
+    }
+
+}
